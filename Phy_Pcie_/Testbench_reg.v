b0ed71e97298549b6b71261bf8ae506eb6e06f7c
kro24alv <kro24alv@gmail.com>
2021-10-05 13:34:51 -0600

    Actualización generador de relojes synth

--- Phy_Pcie_/Testbench_reg.v
+++ Phy_Pcie_/Testbench_reg.v
@@ -1,5 +1,8 @@
 `include "Gen_relojes.v"
 `include "tester_reg.v"
+`include "cmos_cells.v"
+`include "Gen_relojes_synth.v"
+
 `timescale 1ns / 1ps
 // fpga4student.com FPGA projects, VHDL projects, Verilog projects
 // Verilog project: Verilog code for clock divider on FPGA
@@ -11,19 +14,31 @@ module Testbench_reg;
     wire clk_out_f, clk_out_2f, clk_out_4f;
  // Instantiate the Unit Under Test (UUT)
  // Test the clock divider in Verilog
-    Gen_relojes #(.DIVISOR(32'd6)) clk_4f (
+    Gen_relojes #(.DIVISOR(32'd6)) clk_4f_cond (/*AUTOINST*/
             .clk_in     (clk_in), 
             .clk_out    (clk_out_4f)
     );
-    Gen_relojes clk_2f (
+    Gen_relojes clk_2f_cond (/*AUTOINST*/
             .clk_in     (clk_out_4f), 
             .clk_out    (clk_out_2f)
     );
-    Gen_relojes clk_f (
+    Gen_relojes clk_f_cond (/*AUTOINST*/
             .clk_in      (clk_out_2f), 
             .clk_out     (clk_out_f)
     );
-    tester_reg test (
+    Gen_relojes #(.DIVISOR(32'd6)) clk_4f_estr (/*AUTOINST*/
+            .clk_in     (clk_in), 
+            .clk_out    (clk_out_4f)
+    );
+    Gen_relojes clk_2f_estr (/*AUTOINST*/
+            .clk_in     (clk_out_4f), 
+            .clk_out    (clk_out_2f)
+    );
+    Gen_relojes clk_f_estr (/*AUTOINST*/
+            .clk_in      (clk_out_2f), 
+            .clk_out     (clk_out_f)
+    );
+    tester_reg test (/*AUTOINST*/
             .clk_in     (clk_in),
             .clk_out_f  (clk_out_f),
             .clk_out_2f (clk_out_2f),
