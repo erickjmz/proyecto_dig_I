0104c8d6a6221a19bd21236d03c8abee58b49c16
fabio0610 <fabio0610@hotmail.com>
2021-10-22 17:15:39 -0600

    cambios menores

--- serial_paralelo/serial_paralelo.v
+++ serial_paralelo/serial_paralelo.v
@@ -1,75 +1,66 @@
    module serial_paralelo(
-			 input 		  data_in,
-			 output reg [7:0] data_out,
-			 input 		  clk_32f,
+			 input 		  data_intput,
+          input 		  valid_out,
+          input 		  clk_32f,
 			 input 		  clk_4f,
-			 output reg 	  active,
-			 input 		  valid_out,
-			 output reg [2:0] BC_counter
-
+          output reg 	  active_output,
+			 output reg [7:0] data_output,
+			 output reg [2:0] BC_contador
 			 );
-    reg [0:7]				  bus0;
-   
-  reg [2:0]  			  counter;
-
-   
-/*always@(posedge valid_out)begin
-   BC_counter <= 0;
-   counter <=0;
-   
-   end*/
+    reg [0:7]				  bus_0;
+    reg [2:0]  			  contador;
      
 always @(posedge clk_32f) begin
-      if (counter == 0)begin
-   bus0[0] <= data_in;
+      if (contador == 0)begin
+   bus_0[0] <= data_intput;
       end
-   else if (counter == 1) begin
-   bus0[1] <= data_in;
+   else if (contador == 1) begin
+   bus_0[1] <= data_intput;
    end
-    else if (counter == 2) begin
-   bus0[2] <= data_in;
+    else if (contador == 2) begin
+   bus_0[2] <= data_intput;
     end
-    else if (counter == 3) begin
-   bus0[3] <= data_in;
+    else if (contador == 3) begin
+   bus_0[3] <= data_intput;
     end
-    else if (counter == 4) begin
-   bus0[4] <= data_in; //primeros 8bits
+    else if (contador == 4) begin
+   bus_0[4] <= data_intput; //primeros 8bits
     end
-    else if (counter == 5) begin
-   bus0[5] <= data_in;
+    else if (contador == 5) begin
+   bus_0[5] <= data_intput;
     end
-    else if (counter == 6) begin
-   bus0[6] <= data_in;
+    else if (contador == 6) begin
+   bus_0[6] <= data_intput;
     end
-    else if (counter == 7) begin
-   bus0[7] <= data_in;
+    else if (contador == 7) begin
+   bus_0[7] <= data_intput;
     end else begin
-       BC_counter <= 0;   
+       BC_contador <= 0;   
        
     end
-   counter = counter +1;
+   contador = contador +1;
 end
  always @(posedge clk_4f) begin
-    counter <=0;
+    contador <=0;
     
-    data_out <= bus0;
+    data_output <= bus_0;
 
-    if(data_out == 188)begin
-       if(BC_counter == 4)begin
-	  BC_counter <= 4;
+    if(data_output == 188)begin
+       if(BC_contador == 4)begin
+	  BC_contador <= 4;
 	  
        end else begin
 	  
-       BC_counter = BC_counter +1;
+       BC_contador = BC_contador +1;
        end 
     end
 
-    if(BC_counter == 4)begin
-       active <= 1;
+    if(BC_contador == 4)begin
+       active_output <= 1;
     end else begin
-       active <= 0;
+       active_output <= 0;
        end
        
      end // always @ (posedge clk_f)
 
-endmodule // paralelo-serie
+endmodule // serie_paralelo
